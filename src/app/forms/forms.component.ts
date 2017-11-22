7a8e3ec5b72c818d1e1155be097f2d9796f21a67
selvarajChinnasamy <selvaraj1234597@gmail.com>
2017-11-22 17:53:44 +0530

    Check_Commit

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { AbstractControl } from '@angular/forms/src/model';
 
 @Component({
   selector: 'app-forms',
@@ -8,21 +9,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormsComponent implements OnInit {
 
-  names: Array<string> = [];
   nameForm: FormGroup;
   status: boolean = false;
   status1: boolean = false;
   name_input = '';
+  name:Array<string> =['Selvaraj','SelvaKartig'];
   constructor(private fb: FormBuilder) {
+    localStorage.setItem("names",JSON.stringify(this.name));
   }
 
   buildForm() {
     this.nameForm = this.fb.group({
       name: ['', Validators.compose([
         Validators.required,
-        Validators.minLength(2),
-        Validators.pattern('[A-Z][a-z]+[0-9]*'),
-        Validators.pattern("^[^-&$_'\"/;`%]*$")
+        Validators.pattern("^[^!@#+%^()+<>?|'\"/;`%]*$"),
+        Validators.pattern('[A-Z]+[a-z0-9]*'),
+        // Validators.pattern("^[^'\"/;`%]*$"),
+        // this.ValidateName,
       ])],
     });
   }
@@ -30,38 +33,41 @@ export class FormsComponent implements OnInit {
   ngOnInit() {
     this.buildForm();
   }
+ 
+  ValidateName(c: AbstractControl): { [key: string]: boolean} | null
+  {
+    if((c.value!==undefined) && (isNaN(c.value)))
+    {
+      var count=0;
+      var names = JSON.parse(localStorage.getItem("names"));
+      for(let i=0;i<names.length;i++)
+      {
+        if(c.value != names[i])
+        {
+          count++;
+          console.log('names:'+names[i]);
+        }
+        else{
+          break;
+        }
+      }
+    if(count<names.length)
+    {
+      return {error:true};
+    }
+     return null;
+    }
+  }
 
   FormSubmit(data) {
     console.log(data);
-    if (this.CheckNames(data.name)) {
-      this.status1 = true;
-      this.name_input = data.name;
-      setTimeout(() => { this.status1 = false; }, 5000);
-    }
-    else {
-      this.names.push(data.name);
+    var name = JSON.parse(localStorage.getItem("names"));
+      name.push(data.name);
+      localStorage.setItem("names",JSON.stringify(name));
       this.nameForm.reset();
       this.status = true;
       setTimeout(() => { this.status = false; }, 5000);
-    }
   }
 
-  CheckNames(data) {
-    let len = this.names.length;
-    let count = 0;
-    for (let i = 0; i < len; i++) {
-      if (this.names[i] != data) {
-        count++;
-      }
-      else {
-        break;
-      }
-    }
-    if (count < len) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
+ 
 }
